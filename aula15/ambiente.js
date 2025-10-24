b7dfbcdf037d8eb25dcd53a49410f0fbaba713b1
3liax <contato.elias021021@gmail.com>
2025-10-24 10:43:55 -0300

    arrays

---
+++ aula15/ambiente.js
@@ -0,0 +1,11 @@
+let num = [5, 8, 2, 9, 3]
+num.push(1)
+num.sort()
+console.log(num)
+console.log(`o vetor tem ${num.length} posições`)
+let pos = num.indexOf(8)
+if(pos == -1) {
+    console.log('O valor não foi encontrado!')
+}else {
+    console.log(`o valor 8 está na posição ${pos}`)
+    }
